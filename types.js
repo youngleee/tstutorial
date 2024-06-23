773ca856a4e59b972a6a55b93bb7f2bc6b307e31
Young Lee <youngleeee@outlook.com>
2024-06-23 15:16:28 +0200

    types

---
+++ types.js
@@ -0,0 +1,6 @@
+var todo = {
+    description: 'Typescript lernen',
+    status: 'open'
+};
+var text = 'Hallo Welt!';
+console.log(text);
---
+++ types.ts
@@ -0,0 +1,13 @@
+interface Todo {
+    description: string;
+    status: string;
+}
+
+const todo:Todo = {
+    description: 'Typescript lernen',
+    status: 'open'
+};
+
+let text = 'Hallo Welt!';
+
+console.log(text);
